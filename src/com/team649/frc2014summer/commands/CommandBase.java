dc8d8caa7b3e41892fbc32975914395b2379a5aa
Kabi <Kabi@10.56.203.143>
2014-07-13 16:29:34 -0700

    Initial Commit -Kabir

---
+++ src/com/team649/frc2014summer/commands/CommandBase.java
@@ -0,0 +1,48 @@
+package com.team649.frc2014summer.commands;
+
+import com.team649.frc2014summer.OI;
+import com.team649.frc2014summer.RobotMap;
+import com.team649.frc2014summer.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The base for all commands. All atomic commands should subclass CommandBase.
+ * CommandBase stores creates and stores each control system. To access a
+ * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
+ * @author Author
+ */
+public abstract class CommandBase extends Command {
+
+    public static OI oi;
+    // Create a single static instance of all of your subsystems
+    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static Compressor compressor;
+
+    public static void init() {
+        // This MUST be here. If the OI creates Commands (which it very likely
+        // will), constructing it during the construction of CommandBase (from
+        // which commands extend), subsystems are not guaranteed to be
+        // yet. Thus, their requires() statements may grab null pointers. Bad
+        // news. Don't move it.
+        compressor = new Compressor(RobotMap.PRESSURE_SWTICH_CHANNEL, RobotMap.PRESSURE_RELAY_CHANNEL);
+        compressor.start();
+        oi = new OI();
+        
+        // Show what command your subsystem is running on the SmartDashboard
+        SmartDashboard.putData(exampleSubsystem);
+    }
+
+    public static Command oneBallautonomous() {
+        return new 
+        }
+
+    public CommandBase(String name) {
+        super(name);
+    }
+
+    public CommandBase() {
+        super();
+    }
+}
