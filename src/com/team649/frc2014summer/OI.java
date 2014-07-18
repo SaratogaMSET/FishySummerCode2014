b26384502c0dfdd13512c4c646077a40c2b822c4
Kabir <kaju99@gmail.com>
2014-07-17 22:57:32 -0700

    Got some more drivetrain work done... for like the third time... next commit will try to get ready to be tested. also added some work on calculating acceleration to graph
    -Kabir

--- src/com/team649/frc2014summer/OI.java
+++ src/com/team649/frc2014summer/OI.java
@@ -1,5 +1,6 @@
 package com.team649.frc2014summer;
 
+import com.sun.squawk.util.MathUtils;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
@@ -13,14 +14,15 @@ public class OI {
      *
      */
     private Joystick vertical;
-    private Joystick horizontial;
+    private Joystick horizontal;
     private Joystick ShooterJoystick;
+    private double ROTATION_POWER = 1.5;
     public final Driver driver;
     public final Shooter shooter;
 
     public OI() {
         vertical = new Joystick(RobotMap.LEFT_DRIVE_JOYSTICK);
-        horizontial = new Joystick(RobotMap.RIGHT_DRIVE_JOYSTICK);
+        horizontal = new Joystick(RobotMap.RIGHT_DRIVE_JOYSTICK);
         ShooterJoystick = new Joystick(RobotMap.SHOOTER_JOYSTICK);
         driver = new Driver();
         shooter = new Shooter();
@@ -29,11 +31,33 @@ public class OI {
     public class Driver {
 
         public static final double MIN_DRIVE_POWER = 0.05;
-
+        
+         public double getDriveRotation() {
+            final double turnVal = checkJoystickDeadzone(horizontal.getX(), MIN_DRIVE_POWER);
+            final double sign = turnVal < 0 ? -1 : 1;
+            return MathUtils.pow(Math.abs(turnVal), ROTATION_POWER) * sign;
+        } 
+         
+       public double getDriveForward() {
+           double value = -vertical.getY();
+           value = checkJoystickDeadzone(value, MIN_DRIVE_POWER);
+           return value;
+       }
+        
+        public double checkJoystickDeadzone(double value, double deadzone) {
+            if(Math.abs(value) < deadzone) {
+                return 0;
+            }
+            return value;
+        }
+        
+         public boolean isTriggerButtonPressed() {
+            return(vertical.getRawButton(1) || horizontal.getRawButton(1));
+        }
     }
 
     public class Shooter {
-
+        
     }
 
 }
