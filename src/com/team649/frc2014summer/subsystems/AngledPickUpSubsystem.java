e27491d73d1332495680664b21c4c41b3b297b35
Kabir <kaju99@gmail.com>
2014-07-30 20:35:56 -0700

    Figured out and implented a way (non-optimial) to check if pickup is retracted.
    -Kabir
    Signed-off-by:Kabir <kaju99@gmail.com>

--- src/com/team649/frc2014summer/subsystems/AngledPickUpSubsystem.java
+++ src/com/team649/frc2014summer/subsystems/AngledPickUpSubsystem.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.team649.frc2014summer.subsystems;
 
 import com.team649.frc2014summer.RobotMap;
@@ -21,8 +20,8 @@ public class AngledPickUpSubsystem extends Subsystem {
 
     private SpeedController motor;
     private DoubleSolenoid leftPiston;
-    private DoubleSolenoid rightPiston; 
-    
+    private DoubleSolenoid rightPiston;
+
     public static final double PICK_UP_MOTOR_SPEED = -0.4;
     public static final double PURGE_MOTOR_SPEED = 0.4;
     
@@ -30,45 +29,45 @@ public class AngledPickUpSubsystem extends Subsystem {
     private static final Value LEFT_PISTON_RETRACTED_STATE = DoubleSolenoid.Value.kReverse;
     private static final Value RIGHT_PISTON_DEPLOYED_STATE = DoubleSolenoid.Value.kForward;
     private static final Value RIGHT_PISTON_RETRACTED_STATE = DoubleSolenoid.Value.kReverse;
-    
+
     public AngledPickUpSubsystem() {
-        
+
         motor = new Victor(RobotMap.ANGLED_PICK_UP.MOTOR_PORT);
         leftPiston = new DoubleSolenoid(RobotMap.ANGLED_PICK_UP.LEFT_SOLENOID_FORWARD_CHANNEL, RobotMap.ANGLED_PICK_UP.LEFT_SOLENOID_REVERSE_CHANNEL);
         rightPiston = new DoubleSolenoid(RobotMap.ANGLED_PICK_UP.RIGHT_SOLENOID_FORWARD_CHANNEL, RobotMap.ANGLED_PICK_UP.RIGHT_SOLENOID_REVERSE_CHANNEL);
-        
-        
+
     }
-    
+
     public void runMotor(double speed) {
         motor.set(speed);
     }
-    
+
     public void stopMotor() {
         motor.set(0);
     }
-    
-   
-    
+
     public void deployPickup() {
         leftPiston.set(LEFT_PISTON_DEPLOYED_STATE);
         rightPiston.set(RIGHT_PISTON_DEPLOYED_STATE);
     }
-    
+
     public void retractPickup() {
         leftPiston.set(LEFT_PISTON_RETRACTED_STATE);
         rightPiston.set(RIGHT_PISTON_RETRACTED_STATE);
     }
-    
-     public boolean haveBall() {
+
+    public boolean haveBall() {
         return true;
     }
-     
-     public boolean isPickUpRetracted() {
-         return true;
-     }
+
+    public boolean isPickUpRetracted() {
+        
+        //should also have Limit Switch here as the primiary check
+        return(leftPiston.get() == LEFT_PISTON_RETRACTED_STATE && rightPiston.get() == RIGHT_PISTON_RETRACTED_STATE);
+            
+    }
     
     protected void initDefaultCommand() {
     }
-    
+
 }
